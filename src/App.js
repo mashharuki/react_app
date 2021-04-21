8cbb389b4f026e42063a4be8801d1efb4108a9e3
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-21 22:37:43 +0900

    サンプルコンポーネントをコミットします。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Appコンポーネントクラス
+class App extends Component {
+  // レンダリング
+  render () {
+    return <div>
+        <h1 className="bg-primary text-white display-4">
+          React
+        </h1>
+        <div className="container">
+          <p className="subtitle">
+            This is sample component.
+          </p>
+          <p>
+            これはサンプルのコンポーネントです。
+          </p>
+          <p>
+            簡単なメッセージを表示する。
+          </p>
+        </div>
+      </div>
+  }
 }
-
+// Appコンポーネントを外部に出すこと。
 export default App;
