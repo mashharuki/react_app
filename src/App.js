6adcfc893d6a1d4ec77ffb22e31ac1903a94608e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-24 15:26:11 +0900

    子エレメントをコンポーネント化する

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 // Appコンポーネントクラス
 class App extends Component {
-  // 配列データを定義する。
-  data = [
-    "This is list sample",
-    "これはリストのサンプルです。",
-    "配列をリストに変換します。"
-  ]
+  
+  // 変数を初期化する。
+  input = ''
 
   // コンストラクター
   constructor (props) {
     super (props)
-    // ステートの初期化
-    this.state = {
-      list: this.data
-    }
   }
 
   // レンダリング
@@ -26,66 +19,52 @@ class App extends Component {
           React
         </h1>
         <div className="container">
-          <p className="subtitle">
-            Show List.
-          </p>
-          <List title="サンプル・リスト" data={this.data} />
+          <Message title="Children">
+            これはコンポーネント内のコンテンツです。
+            マルチでテキストを分割し、リストにして表示します。
+            改行は必要ありません。
+          </Message>
         </div>
       </div>
   }
 }
 
-// Listコンポーネントクラス
-class List extends Component {
-  // 変数を初期化する。
-  number = 1
-
-  // レンダリングする。
-  render () {
-    // ステートの値を格納する。
-    let data = this.props.data
-    return (
-      <div>
-        <p className="h5 text-center">
-          {this.props.title}
-        </p>
-        <ul className="list-group">
-          {data.map((item, key) => 
-            <li className="list-group-item" key={key}>
-              <Item number={key + 1} value={item} />
-            </li>
-          )}
-        </ul>
-      </div>
-    )
-  }
-}
-
-// Itemコンポーネントクラス
-class Item extends Component {
-
-  itm = {
-    fontsize : "16pt",
-    color : "#00f",
-    textDecoration : 'underline',
-    textDecorationColor : '#ddf'
-  }
-
-  num = {
+// Messageコンポーネントクラス
+class Message extends Component {
+  
+  li = {
+    fontSize : "14pt",
     fontWeight : "bold",
-    color : "red"
+    color : '#090'
   }
 
-  // レンダリングする。
+  // レンダリング
   render () {
-    return (
-      <p style={this.itm}>
-        <span style={this.num}>
-          [{this.props.number}]&nbsp;
-        </span>
-        {this.props.value}
-      </p>
-    )
+    // 変数を初期化
+    let content = this.props.children
+    let arr = content.split('。')
+    let arr2 = []
+    // 配列arr2に値を詰める。
+    for (let i = 0;i < arr.length; i++ ) {
+      if (arr[i].trim() != '') {
+        arr2.push(arr[i]);
+      }
+    }
+    // arr2の値を使ってmapを作成する。
+    let list = arr2.map( (value, key) => (
+      <li className="list-group-item" style={this.li} key={key}>
+        {key + 1}. {value}.
+      </li>
+    ))
+      
+    return <div>
+        <h2>
+          {this.props.title}
+        </h2>
+        <ol className="list-group">
+          {list}
+        </ol>
+      </div>
   }
 }
 
