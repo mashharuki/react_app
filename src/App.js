de3f3e262e8881bacc6e7401ad62cf94a97cd890
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-24 16:58:59 +0900

    コンテキストを使用した内容に更新する。

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// 変数を初期化
+let data = {
+  title : 'React-Context',
+  message : 'this is sample message.'
+}
+
+// コンテキストを用意する。
+const SampleContext = React.createContext(data)
+
 // Appコンポーネントクラス
 class App extends Component {
   
-  // 変数を初期化する。
-  input = ''
-
-  // コンストラクター
-  constructor (props) {
-    super (props)
-    this.state = {
-      title : 'input form',
-      message : 'type your name.',
-      max : 10,
-    }
-    // メソッドをバインドする。
-    this.doCheck = this.doCheck.bind(this)
-  }
-
-  // チェックするための関数
-  doCheck (event) {
-    // アラートを表示する。
-    alert(event.target.value + "は長すぎます。(最大" + this.state.max + "文字)")
-  }
-
   // レンダリング
   render () {
     return <div>
@@ -32,47 +20,44 @@ class App extends Component {
           React
         </h1>
         <div className="container">
-          <h4>
-            {this.state.title}
-          </h4>
-          <Message maxlength={this.state.max} onCheck={this.doCheck} />
+          <Title />
+          <Message />
         </div>
       </div>
   }
 }
 
+// Titleコンポーネントクラス
+class Title extends Component {
+  // コンテキストを設定する。
+  static contextType = SampleContext
+
+  // レンダリングする。
+  render () {
+    return (
+      <div className="card p-2 my-3">
+        <h2>
+          {this.context.title}
+        </h2>
+      </div>
+    )
+  }
+}
+
 // Messageコンポーネントクラス
 class Message extends Component {
-  
-  li = {
-    fontSize : "14pt",
-    fontWeight : "bold",
-    color : '#090'
-  }
-
-  // コンストラクター
-  constructor (props) {
-    super(props)
-    // メソッドをバインドする。
-    this.doChange = this.doChange.bind(this)
-  }
-
-  // 変更するための関数
-  doChange (e) {
-    if (e.target.value.length > this.props.maxlength) {
-      // チェック関数を実行する。
-      this.props.onCheck(e)
-      // 強制的に10文字に変更させる。
-      e.target.value = e.target.value.substr(0, this.props.maxlength)
-    }
-  }
+  // コンテキストを設定する。
+  static contextType = SampleContext
 
   // レンダリング
   render () {
-    return <div className="form-group">
-       <label>input:</label>
-       <input type="text" className="form-control" onChange={this.doChange} />
+    return (
+      <div className="alert alert-primary">
+       <p>
+        {this.context.message}
+       </p>
       </div>
+    )
   }
 }
 
