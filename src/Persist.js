c866c75776e7502e40fb02c81dc0a73850e4e871
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-01 21:44:04 +0900

    Persist.jsを追加する。

---
+++ src/Persist.js
@@ -0,0 +1,35 @@
+// ローカルストレージに保管するフック
+import React, { useState } from 'react';
+
+// 独自フック(ky: 変数, initVal:関数)
+function usePersist(ky, initVal) {
+    // ステート変数を用意する。
+    const [ savedValue, setSavedValue ] = useState(value);
+    // キー値
+    const key = "hooks:" + ky;
+    // ローカルストレージから値を取得する関数
+    const value = () => {
+        try {
+            // ローカルストレージからキー値に該当する値を取得
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initVal;
+        } catch (err) {
+            console.log(err);
+            return initVal;
+        }
+    }
+    // ローカルストレージに値を保管する関数
+    const setValue = (val) => {
+        try {
+            // ローカルストレージに保管する。
+            setSavedValue(val);
+            window.localStorage.setItem(key, JSON.stringify(val));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    return [ savedValue, setValue ];
+}
+
+// コンポーネントを外部に公開
+export default usePersist;
