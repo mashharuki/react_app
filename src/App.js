10834b3778fe901582a355039328a2f79459bd3d
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-01 17:27:14 +0900

    副作用フックを追加しました。

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// Appコンポーネントクラス
+// アラートを表示する関数コンポーネント
+function AlertMessage(props) {
+  // ステート変数
+  const data = props.data;
+  const msg = JSON.stringify(data);
+
+  // レンダリング
+  return <div className="alert alert-primary h5 text-primary">
+    <h5>
+      {props.msg}
+    </h5>
+  </div>
+}
+
+// App関数コンポーネント
 function App() {
   // ステートフックを設定
-  const [message] = useState("Welcome to Hooks!");
+  const [ name, setName ] = useState("");
+  const [ mail, setMail ] = useState("");
+  const [ age, setAge ] = useState(0);
+  const [ form, setForm ] = useState({ 
+    name: 'no name', 
+    mail: 'no mail', 
+    age: 0 
+  });
+  const [ val, setVal ] = useState(0);
+  const [ msg, setMsg ] = useState('set a number...');
+
+  // 以下、それぞれ値を更新する関数
+  const doChangeName = (event) => {
+    setName(event.target.value);
+  }
+
+  const doChangeMail = (event) => {
+    setMail(event.target.value);
+  }
+
+  const doChangeAge= (event) => {
+    setAge(event.target.value);
+  }
+
+  const doChange = (event) => {
+    setVal(event.target.value);
+  }
+
+  // 送信するための関数
+  const doSubmit = (event) => {
+    // ステートをセットする。
+    setForm({name: name, mail: mail, age: age});
+    event.preventDefault();
+  }
+
+  // 副作用フック(更新時に指定の関数を実行する。)
+  useEffect (() => {
+    let total = 0;
+    // 合計回数だけループする。
+    for (let i = 0;i < val; i++){
+      total += 1;
+    }
+    // ステートを更新する。
+    setMsg("total: " + total + ".");
+  });
 
   // レンダリング
   return (
@@ -16,15 +74,30 @@ function App() {
         <h4 className="my-3">
           Hooks sample
         </h4>
-        <div className="alert alert-primary text-center">
-          <p className="h5">
-            {message}.
-          </p>
+        <AlertMessage msg={msg} />
+        <div className="form-group">
+          <label>Inpit:</label>
+          <input type="number" className="form-control" onChange={doChange}/>
         </div>
+        <form onSubmit={doSubmit}>
+          <div className="form-group">
+            <label>Name:</label>
+            <input type="text" className="form-control" onChange={doChangeName}/>
+          </div>
+          <div className="form-group">
+            <label>Mail:</label>
+            <input type="text" className="form-control" onChange={doChangeMail}/>
+          </div>
+          <div className="form-group">
+            <label>Age:</label>
+            <input type="text" className="form-control" onChange={doChangeAge}/>
+          </div>
+          <input tyoe="submit" className="btn btn-primary" value="Click" />
+        </form>
       </div>
     </div>
   );
 }
 
 // Appコンポーネントを外部に出すこと。
-export default App;
+export default App; 
