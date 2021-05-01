5bc20664d380ae4234ae2a5d2df4a6624329acab
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-01 12:03:29 +0900

    ステートフックを使用したコードに修正しました。

--- src/App.js
+++ src/App.js
@@ -1,87 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-// 変数を初期化
-let theme = {
-  light: {
-    styles: {
-      backgroundColor: "#f0f9ff",
-      color: "#00f",
-    },
-    head: "bg-primary text-white display-4 mb-4",
-    alert: "alert alert-primary my-3",
-    text: "text-primary m-3",
-    foot: "py-4",
-  },
-  dark: {
-    styles: {
-      backgroundColor: "#336",
-      color: "#eef",
-    },
-    head: "bg-secondary text-white display-4 mb-4",
-    alert: "alert alert-dark my-3",
-    text: "text-light m-3",
-    foot: "py-4",
-  }
-}
-
-// コンテキストを用意する。
-const ThemeContext = React.createContext(theme.dark)
-
 // Appコンポーネントクラス
-class App extends Component {
+function App() {
+  // ステートフックを設定
+  const [message] = useState("Welcome to Hooks!");
 
-  // コンテキストを設定する。
-  static contextType = ThemeContext
-  
   // レンダリング
-  render () {
-    return <div style={this.context.styles} >
-        <h1 className={this.context.head}>
-          React
-        </h1>
-        <div className="container">
-          <Title value="Content page" />
-          <Message value="This is Content sample." />
-          <Message value="※これはテーマのサンプルです。" />
-          <div className={this.context.foot}></div>
+  return (
+    <div>
+      <h1 className="bg-primary text-white display-4">
+        React
+      </h1>
+      <div className="container">
+        <h4 className="my-3">
+          Hooks sample
+        </h4>
+        <div className="alert alert-primary text-center">
+          <p className="h5">
+            {message}.
+          </p>
         </div>
       </div>
-  }
-}
-
-// Titleコンポーネントクラス
-class Title extends Component {
-  // コンテキストを設定する。
-  static contextType = ThemeContext
-
-  // レンダリングする。
-  render () {
-    return (
-      <div className={this.context.alert}>
-        <h2 style={this.context.style}>
-          {this.props.value}
-        </h2>
-      </div>
-    )
-  }
-}
-
-// Messageコンポーネントクラス
-class Message extends Component {
-  // コンテキストを設定する。
-  static contextType = ThemeContext
-
-  // レンダリング
-  render () {
-    return (
-      <div className={this.context.style}>
-       <p className={this.context.text}>
-        {this.props.value}
-       </p>
-      </div>
-    )
-  }
+    </div>
+  );
 }
 
 // Appコンポーネントを外部に出すこと。
