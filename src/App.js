3c52570dcdb2fdca7adf26194ae1ca2cdae41645
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-02 11:23:02 +0900

    土台となるApp.jsを更新しました。

--- src/App.js
+++ src/App.js
@@ -1,59 +1,6 @@
-import React, { useState, useEffect } from 'react';
+// ベースとなるApp.js
 import './App.css';
-import usePersist from './Persist';
-
-// 合計計算用の関数コンポーネント
-function AlertMessage(props) {
-  // ステートフックを設定
-  const [ name, setName ] = useState("");
-  const [ mail, setMail ] = useState("");
-  const [ age, setAge ] = useState(0);
-  const [ mydata, setMydata ] = usePersist("mydata", null);
-
-  // 以下、それぞれ値を更新する関数
-  const onChangeName = (event) => {
-    setName(event.target.value);
-  }
-
-  const onChangeMail = (event) => {
-    setMail(event.target.value);
-  }
-
-  const onChangeAge = (event) => {
-    setAge(event.target.value);
-  }
-
-  const onAction = (e) => {
-    const data = {
-      name: name,
-      mail: mail,
-      age: age
-    }
-    // ステート変数を更新する。
-    setMydata(data);
-  }
-
-  return <div className="alert alert-primary h5 text-primary">
-    <h5 className="mb-4">
-      {JSON.stringify(mydata)}
-    </h5>
-    <div className="form-group">
-      <label className="h6">Name:</label>
-      <input type="text" className="form-control" onChange={onChangeName}/>
-    </div>
-    <div className="form-group">
-      <label className="h6">Mail:</label>
-      <input type="mail" className="form-control" onChange={onChangeMail}/>
-    </div>
-    <div className="form-group">
-      <label className="h6">Age:</label>
-      <input type="number" className="form-control" onChange={onChangeAge}/>
-    </div>
-    <button className="btn btn-primary" onClick={onAction}>
-      Saved it!
-    </button>
-  </div>
-}
+import MemoPage from './memo/MemoPage';
 
 // App関数コンポーネント
 function App() {
@@ -66,9 +13,9 @@ function App() {
       </h1>
       <div className="container">
         <h4 className="my-3">
-          Hooks sample
+          Memo.
         </h4>
-        <AlertMessage />
+        <MemoPage />
       </div>
     </div>
   );
