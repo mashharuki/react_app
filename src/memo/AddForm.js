02baac70edbd6cc0ebbb489da74164fba07928b6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-05-02 12:24:26 +0900

    AddFormコンポーネントを更新しました。

---
+++ src/memo/AddForm.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import usePersist from '../Persist';
+
+// AddFormコンポーネント
+function AddForm (props) {
+    // ステート変数を用意する。
+    const [ memo, setMemo ] = usePersist("mode", []);
+    const [ message, setMessage ] = useState('');
+
+    // doChange関数
+    const doChange = (e) => {
+        // ステート変数を更新する。
+        setMessage(e.target.value);
+    }
+
+    // doAction関数
+    const doAction = (e) => {
+        // 配列データを作成
+        const data = {
+            message: message,
+            created: new Date()
+        };
+        // memo配列に追加する。
+        memo.unshift(data);
+        // ステート変数を更新する。(独自フック呼び出してローカルストレージに保存する。)
+        setMemo(memo);
+        setMessage('');
+    }
+
+    // レンダリング
+    return (
+        <form onSubmit={doAction} action="">
+            <div className="form-control row">
+                <input type="text" className="form-control-sm col" onChange={doChange} value={message} required/>
+                <input type="submit" className="btn btn-primary btn-sm col-2" value="Add"/>
+            </div>
+        </form>
+    );
+}
+
+export default AddForm;
